d5e6c5eb05e4a483835e73577d8e967102cc1b6d
lookinALX <67375219+lookinALX@users.noreply.github.com>
2023-02-05 15:59:42 +0100

    Add files via upload

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from discord import Intents
+from discord import Intents, Embed
 from discord.ext.commands import Bot as BotBase
 import telegram
 import config
@@ -42,6 +42,10 @@ class Bot(BotBase):
             self.ready = True
             self.guild = self.get_guild(config.GUILD_ID)
             print("bot ready")
+            print(f"Guild: {self.guild}")
+
+            channel_status = self.get_channel(config.STATUS_CHANNEL)
+            await channel_status.send("I'm now online!")
         else:
             print("bot reconnected")
 
@@ -49,6 +53,18 @@ class Bot(BotBase):
         if message.channel.id in config.channelIDsToListen:
             print(f'Message from {message.author}: {message.content}')
             await TG.sendMessage(config.TEL_CHANNEL_ID, message.content)
+
+        if message.channel.id in [1071798085098934363] and message.author != self.user:
+            wellcome_embed = Embed(title="Greetings my friend!", description=f'Добро пожаловать {message.author}!',
+                                   colour=0x66BB55)
+
+            wellcome_embed.add_field(name="Присоединяйся в Telegram канал, чтобы видеть все анонсы",
+                                     value="https://t.me/+eCyurNGK8A4yZDRi", inline=False)
+
+            wellcome_embed.set_author(name="ladAgent", icon_url=self.guild.icon.url)
+            channel = self.get_channel(1071798085098934363)
+            await channel.send(embed=wellcome_embed)
+            await channel.send(f"Нас уже {self.guild.member_count} подпивасов")
 
 
 bot = Bot()
